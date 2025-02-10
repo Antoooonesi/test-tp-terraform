ce16d705443b63c278458dbd1cbddec72fde0c05
Antoone <antoooonesi@gmail.com>
2025-02-10 14:01:59 +0100

    inshallah

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_route_table_association" "private_subnet_assoc" {
 }
 
 # Instance Ubuntu (bastion)
-resource "aws_instance" "bastion" {
+resource "aws_instance" "bastion2" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
@@ -136,7 +136,7 @@ resource "aws_instance" "bastion" {
 }
 
 # Instance application (HTTPD)
-resource "aws_instance" "application" {
+resource "aws_instance" "application2" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
